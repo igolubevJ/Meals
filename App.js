a7555269f3ac43e6b4c491c1643f437d11debcf0
igolubevJ <igolubevadd@gmail.com>
2021-06-23 15:25:43 +0300

    Add Android Status bar Safe area

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar, StyleSheet, SafeAreaView, View, Text, Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
+console.log();
 
 export default function App() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? StatusBar.currentHeight: 0 }}>
+        <View style={{ padding: 16, backgroundColor: "lightgreen" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "lightblue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
