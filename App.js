f1c719c2ef6bec49b739a852727faec6e6725088
igolubevJ <igolubevadd@gmail.com>
2021-06-23 16:53:51 +0300

    Create simple restaurant info card

--- App.js
+++ App.js
@@ -1,47 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Text,
-  Platform,
-} from "react-native";
-import { Searchbar } from "react-native-paper";
 
-const isAndroid = Platform.OS === "android";
-
-console.log();
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: isAndroid ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "lightgreen",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "lightblue",
-  },
-});
