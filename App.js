0777f4367356effc92f02828a767b20057ad9a3a
igolubevJ <igolubevadd@gmail.com>
2021-06-24 15:24:44 +0300

    Add loading custom fonts

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infastructure/theme";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
